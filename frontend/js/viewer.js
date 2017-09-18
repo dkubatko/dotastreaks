7a7fa4aa6ca63c124977d55fd004d109b627c71e
Daniil <dkubatko@ucsd.edu>
2017-09-18 13:51:56 -0600

    Working on style

--- frontend/js/viewer.js
+++ frontend/js/viewer.js
@@ -34,25 +34,7 @@ if (window.Twitch.ext) {
             dataType: 'json',
             data: JSON.stringify(data),
             success: function(data) {
-                  console.log(data.Choice[0])
-                  if (!data.Choice[0]) {
-                      $("#block1").fadeOut("slow");
-                  } else {
-                      $("#block1").fadeIn("slow");
-                  }
-                  if (!data.Choice[1]) {
-                      $("#block2").fadeOut("slow");
-                  } else {
-                      $("#block2").fadeIn("slow");
-                  }
-                  if (!data.Choice[2]) {
-                      $("#block3").fadeOut("slow");
-                  } else {
-                      $("#block3").fadeIn("slow");
-                  }
-                  $("#val1").text(data.Streak)
-                  $("#val2").text(data.Kills)
-                  $("#val3").text(data.Deaths)
+                      trackData(data);
             },
             complete: function() {
               // Schedule the next request when the current one's complete
@@ -61,6 +43,50 @@ if (window.Twitch.ext) {
         }); 
     })();
   });
+}
+
+var goodStats = "rgb(102, 255, 153)"
+var regStats = "rgb(255, 255, 255)"
+
+function trackData(data) {
+    console.log(data.Choice[0])
+    //get rid of non-tracked data
+    if (!data.Choice[0]) {
+      $("#block1").fadeOut("slow");
+    } else {
+      $("#block1").fadeIn("slow");
+    }
+    if (!data.Choice[1]) {
+      $("#block2").fadeOut("slow");
+    } else {
+      $("#block2").fadeIn("slow");
+    }
+    if (!data.Choice[2]) {
+      $("#block3").fadeOut("slow");
+    } else {
+      $("#block3").fadeIn("slow");
+    }
+    //now track data
+    $("#val1").text(data.Streak)
+    if (data.Streak > 0) {
+        $("#val1").animate({backgroundColor: goodStats}, "slow");
+    } else {
+        $("#val1").animate({backgroundColor: regStats}, "slow");
+    }
+
+    $("#val2").text(data.Kills)
+    if (data.Kills > 10) {
+        $("#val2").animate({backgroundColor: goodStats}, "slow");
+    } else {
+        $("#val2").animate({backgroundColor: regStats}, "slow");
+    }
+    
+    $("#val3").text(data.Deaths)
+    if (data.Deaths == 0) {
+        $("#val3").animate({backgroundColor: goodStats}, "slow");
+    } else {
+        $("#val3").animate({backgroundColor: regStats}, "slow");
+    }
 }
 
 
