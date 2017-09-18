419fef2e939ac910b3fb3dff78dece223000b722
Daniil <dkubatko@ucsd.edu>
2017-09-17 18:25:19 -0600

    user update endpoint added

--- frontend/js/viewer.js
+++ frontend/js/viewer.js
@@ -15,28 +15,27 @@ or in the "license" file accompanying this file. This file is distributed on an 
 */
 
 $(document).ready(function() {
-    update();
-});
-
-(function update() {
-    var data = {
+    (function update() {
+      var data = {
                 "channel_id": Gauth.channelId
             };
-   $.ajax({
-        url: 'https://dotastreaks.com/userUpdate',
-        type: 'POST',
-        headers: {
-            'x-extension-jwt': Gauth.token
-        },
-        contentType: "application/json; charset=utf-8",
-        dataType: 'json',
-        data: JSON.stringify(data),
-        success: function(data) {
-          console.log(data);
-        },
-        complete: function() {
-          // Schedule the next request when the current one's complete
-          setTimeout(update, 5000);
-        }
-    }); 
+       $.ajax({
+            url: 'https://dotastreaks.com/userUpdate',
+            type: 'POST',
+            headers: {
+                'x-extension-jwt': Gauth.token
+            },
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            data: JSON.stringify(data),
+            success: function(data) {
+              console.log(data);
+            },
+            complete: function() {
+              // Schedule the next request when the current one's complete
+              setTimeout(update, 5000);
+            }
+        }); 
+    })();
 });
+
