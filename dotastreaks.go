5e8fc8ff05222d05a67902c70464b52783663862
Daniil <dkubatko@ucsd.edu>
2017-09-17 23:36:57 -0600

    Fixed redundant prints

--- dotastreaks.go
+++ dotastreaks.go
@@ -139,8 +139,6 @@ func (u *User) save() error {
 
 		choice.Put([]byte(u.Client_id), buf.Bytes())
 
-		fmt.Println(choice.Get([]byte(u.Client_id)))
-
 		return nil
 	})
 	return nil
@@ -620,44 +618,42 @@ func configDone(rw http.ResponseWriter, req *http.Request) {
 	fmt.Println(updUser.Stats.Choice)
 	updUser.save()
 
-	/*
-		var signature = JWTSignature{Exp: 1505774570, User_id: "43665292",
-			Role: "external"}
-		tokenstr, err := signToken(signature)
+	var signature = JWTSignature{Exp: 1505774570, User_id: "43665292",
+		Role: "external"}
+	tokenstr, err := signToken(signature)
 
-		if err != nil {
-			return
-		}
+	if err != nil {
+		return
+	}
 
-		jsonStr, _ := json.Marshal(ConfigResp{"done"})
+	jsonStr, _ := json.Marshal(ConfigResp{"done"})
 
-		fmt.Println(string(jsonStr))
+	fmt.Println(string(jsonStr))
 
-		url := "https://api.twitch.tv/extensions/277906/0.0.1/required_configuration"
+	url := "https://api.twitch.tv/extensions/277906/0.0.1/required_configuration"
 
-		auth := "Bearer " + tokenstr
+	auth := "Bearer " + tokenstr
 
-		r, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonStr))
-		r.Header.Set("Authorization", auth)
-		r.Header.Set("Client-Id", "ebfbsgj6lg9k2d4czcycledd89vrz9")
-		r.Header.Set("Content-Type", "application/json")
+	r, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonStr))
+	r.Header.Set("Authorization", auth)
+	r.Header.Set("Client-Id", "ebfbsgj6lg9k2d4czcycledd89vrz9")
+	r.Header.Set("Content-Type", "application/json")
 
-		q := r.URL.Query()
-		q.Add("channel_id", val.Channel_id)
-		r.URL.RawQuery = q.Encode()
+	q := r.URL.Query()
+	q.Add("channel_id", val.Channel_id)
+	r.URL.RawQuery = q.Encode()
 
-		resp, err := (&http.Client{}).Do(r)
+	resp, err := (&http.Client{}).Do(r)
 
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 
-		fmt.Println(string(body))
-	*/
+	fmt.Println(string(body))
 
 }
 
