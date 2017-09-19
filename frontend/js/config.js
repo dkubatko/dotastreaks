862f7b1197166e683b5809283faa4e2f42d099f2
Daniil <dkubatko@ucsd.edu>
2017-09-18 23:57:44 -0700

    Links fixed

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -110,7 +110,6 @@ function sendSteamID() {
 }
 
 function successVerify(data) {
-    console.log(data)
     $("#loading").slideUp("slow");
     if (data.Response == "ok") {
         $("#resimg").delay(100).attr("src", "https://dotastreaks.com/images/ok.png");
@@ -125,5 +124,4 @@ function successVerify(data) {
 
 function notSuccess() {
     $("#loading").slideUp("slow");
-    console.log("Error")
 }
