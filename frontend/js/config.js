b2df43291b1e10208c695d324b92e6221267b0ed
Daniil Kubatko <dankubatko@gmail.com>
2017-09-27 21:06:38 -0700

    removed redundant common js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -8,6 +8,13 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
+var Gauth;
+if (window.Twitch.ext) {
+  window.Twitch.ext.onAuthorized(function(auth) {
+      Gauth = auth;
+  });
+}
+
 //constant colors for change
 var pressedColor = "rgb(102, 255, 153)"
 var unpressedColor = "rgb(255, 255, 255)"
