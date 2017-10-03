4c9fa355b79f13eec03292e8104aa4a53d6cecd7
Daniil Kubatko <dankubatko@gmail.com>
2017-10-03 13:10:56 -0700

    fixed empty array of bytes

--- dotastreaks.go
+++ dotastreaks.go
@@ -293,7 +293,7 @@ func readAll() ([]User, error) {
 				us.Stats.Choice = toBool(stats)
 			}
 
-			if best_streak := bstr.Get([]byte(k)); best_streak != nil {
+			if best_streak := bstr.Get([]byte(k)); best_streak != nil && best_streak != [] {
 				fmt.Println(best_streak)
 				us.Stats.Best_streak = int(binary.BigEndian.Uint64(best_streak))
 				log.Printf("%v is beststreak for %v\n", us.Stats.Best_streak, us.Channel_id)
