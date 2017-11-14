2177a399c453d6519479c182a2b0befc7e9b68e8
Daniil Kubatko <dankubatko@gmail.com>
2017-11-14 00:45:49 -0800

    Fixed the problem with the request limit

--- dotastreaks.go
+++ dotastreaks.go
@@ -769,7 +769,7 @@ func launchUpdates() {
 			log.Printf("Cycle %v has gone through. sleepTime is %v. Status: ok.\n", count, sleepTime)
 		}
 
-		time.Sleep(sleepTime * time.Second)
+		time.Sleep(time.Duration(sleepTime) * time.Second)
 	}
 }
 
